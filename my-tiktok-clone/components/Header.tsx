37fbf415eb9410a68a75118144ca70e24b4aa62d
BHObserver <BHObserver0@gmail.com>
2024-05-21 18:18:43 +0600

    Create Header.tsx

---
+++ my-tiktok-clone/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => (
+    <div className="flex justify-between items-center p-4 bg-black border-b border-gray-800">
+      <div className="text-2xl font-bold text-white"><svg fill="currentColor" font-size="24px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"><path d="M5 11a1 1 0 0 1 1-1h36a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-2Zm0 12a1 1 0 0 1 1-1h36a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-2Zm1 11a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h36a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1H6Z"></path></svg></div>
+      <div className="text-2xl text-white"><svg fill="currentColor" font-size="24px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"><path d="M21.26 7a15.26 15.26 0 1 0 0 30.53 15.26 15.26 0 0 0 0-30.53ZM2 22.26A19.26 19.26 0 1 1 36.23 34.4l9.65 9.66c.3.29.3.76 0 1.06l-1.76 1.76c-.3.3-.77.3-1.06 0l-9.66-9.65A19.26 19.26 0 0 1 2 22.27Z"></path></svg></div>
+    </div>
+  );
+  
+  export default Header;
+  
