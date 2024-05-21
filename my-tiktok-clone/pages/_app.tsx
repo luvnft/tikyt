d33b36f63108382fce201496eba75fcf62c9568e
BHObserver <BHObserver0@gmail.com>
2024-05-22 03:02:22 +0600

    Update _app.tsx

--- my-tiktok-clone/pages/_app.tsx
+++ my-tiktok-clone/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import Layout from '../components/Layout'; // Adjust the path according to your file structure
+import { FC } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface MyAppProps {
+  Component: FC;
+  pageProps: Record<string, any>; // Adjust the type according to your pageProps structure
+}
+
+const MyApp: FC<MyAppProps> = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
